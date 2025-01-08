d23e7a3e81537cbc98c656d44515d547d603ebdb
gtryiyi <94560494+gtryiyi@users.noreply.github.com>
2025-01-08 19:59:33 +0100

    twitch-login.js

--- api/twitch-login.js
+++ api/twitch-login.js
@@ -1,6 +1,6 @@
 export default (req, res) => {
-  const CLIENT_ID = 'your_twitch_client_id'; // Replace with your Twitch Client ID
-  const REDIRECT_URI = 'https://your-vercel-app.vercel.app/api/twitch-callback'; // Replace with your app URL
+  const CLIENT_ID = '5jmz2zhpvhpe6pgenh9didik787tvr'; // Replace with your Twitch Client ID
+  const REDIRECT_URI = 'https://quizwow.vercel.app/api/twitch-callback'; // Replace with your app URL
 
   const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=`;
 
